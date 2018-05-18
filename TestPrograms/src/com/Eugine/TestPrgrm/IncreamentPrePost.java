0b4c1906d923f68a294246153d22f6870d10a8d6
udhay <udhay@EugIsai>
2018-05-18 07:25:35 +0530

    IncreamnetAdded

---
+++ TestPrograms/src/com/Eugine/TestPrgrm/IncreamentPrePost.java
@@ -0,0 +1,15 @@
+package com.Eugine.TestPrgrm;
+
+public class IncreamentPrePost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b;
+		b=a++;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
