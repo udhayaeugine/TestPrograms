8d5ea7423ce491ec89a6299c5b120bb221e28505
Eugine L <Eugine L@EugisDell>
2023-12-10 21:27:19 +0000

    Code Aligned

--- src/testCodePackage/FileScrapperUtility.java
+++ src/testCodePackage/FileScrapperUtility.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-
 /*
  * Purpose of this program is to read substring from a file and write the substring on another.
  * Author : Udhaya Eugine Loganathan
@@ -19,29 +18,27 @@ import java.util.Scanner;
 public class FileScrapperUtility {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
+
 		BufferedReader br = new BufferedReader(new FileReader("..//Test//LoanNumber.txt"));
 		FileWriter br1 = new FileWriter("..//Test//LoanNumber1.txt");
-        PrintWriter pw=new PrintWriter(br1);
-        
-		String line = null; 
+		PrintWriter pw = new PrintWriter(br1);
+
+		String line = null;
 		try (Scanner sc = new Scanner(br)) {
-			try{
-				while(sc.hasNextLine()) {
+			try {
+				while (sc.hasNextLine()) {
 					line = sc.nextLine();
-					pw.write(line.substring(28,39)+"\n");
+					pw.write(line.substring(28, 39) + "\n");
 				}
 				br.close();
 				pw.close();
 				br1.close();
-				
-			}catch(IOException io) {
+
+			} catch (IOException io) {
 				System.out.println(io.getMessage());
 			}
 		}
 
+	}
 
 }
-
-}
-
